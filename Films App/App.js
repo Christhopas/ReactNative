5ee1a1cf3dd8bb4dc522e5d09a79f767c714a2a0
Christopher Thompson <101826879+Christhopas@users.noreply.github.com>
2023-05-20 15:46:46 -0300

    Update App.js

--- Films App/App.js
+++ Films App/App.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginPage from "./src/pages/LoginPage";
 import RegisterPage from "./src/pages/RegisterPage";
+import FilmPage from './src/Pages/FilmPage';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Página de Login" component={LoginPage}/>
+        <Stack.Screen name = "Página de Filme" component={FilmPage}/>
         <Stack.Screen name="Página de Registro" component={RegisterPage}/>
       </Stack.Navigator>
     </NavigationContainer>
